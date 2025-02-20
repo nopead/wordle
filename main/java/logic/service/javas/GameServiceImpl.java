5abd5f4bba551e2aecd9068809d46cb6f7d48bf8
nopead <smirnov.smirnov2003@yandex.ru>
2025-02-20 19:02:30 +0300

    Game Service changes

--- main/java/logic/service/javas/GameServiceImpl.java
+++ main/java/logic/service/javas/GameServiceImpl.java
@@ -1,25 +1,44 @@
 package main.java.logic.service.classes;
 
 import main.java.logic.service.classes.IGameService;
-import main.java.logic.game.classes.GameSession;
+import main.java.logic.game.classes.Game;
+import main.java.logic.game.classes.WordInDictionaryChecker;
+import main.java.logic.game.classes.DictionaryGetter;
 
 public class GameServiceImpl implements IGameService{
 	
-	private GameSession gameSession;
+	private Game game;
 	
 	public void startGame(){
-		gameSession = new GameSession();
+		game = new Game();
 	}
 	
 	public boolean isAttemptsOver(){
-		return gameSession.remainingAttemptsAnalyzer.isAttemptsOver();
+		return game.isAttemptsOver();
 	}
 	
 	public boolean isGuessWordIsAnswer(String guessWord){
-		return gameSession.guessWordAnalyzer.compareWithSecret(guessWord);
+		return game.compareWithSecret(guessWord);
+	}
+	
+	public boolean isUserInputIsWord(String input){
+		String[] dictionary = DictionaryGetter.getDictionary();
+		return WordInDictionaryChecker.isExists(dictionary, input);
 	}
 	
 	public String getLettersStates(){
 		return "";
+	}
+	
+	public void decreaseRemainingAttemptsCount(){
+		game.decreaseRemainingAttemptsCount();
+	}
+	
+	public int getRemainingAttemptsCount(){
+		return game.getRemainingAttemptsCount();
+	}
+	
+	public void stateLetters(String guessWord){
+		game.stateLetters(guessWord);
 	}
 }
--- main/java/logic/service/javas/IGameService.java
+++ main/java/logic/service/javas/IGameService.java
@@ -2,12 +2,20 @@ package main.java.logic.service.classes;
 
 public interface IGameService{
 	
+	public int getRemainingAttemptsCount();
+	
 	public void startGame();
+	
+	public void decreaseRemainingAttemptsCount();
+	
+	public void stateLetters(String guessWord);
 	
 	public String getLettersStates();
 	
 	public boolean isAttemptsOver(); 
 	
 	public boolean isGuessWordIsAnswer(String guessWord);
+	
+	public boolean isUserInputIsWord(String input);
 	
 }
