6c7ab103b4bb6e9bb88b050e1f14810e8111cb8d
Александр Смирнов <smirnov.ai@mmtr.ru>
2025-02-28 10:31:32 +0300

    added exception handling in program start point

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
-import main.view.Wordle;
+import main.java.view.Wordle;
 
 public class Main{
+	
 	public static void main(String[] args){
-		Wordle wordle = new Wordle();
-		wordle.run();
+		try{
+			Wordle wordle = new Wordle();
+			wordle.run();
+		} catch (Exception e){
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
 	}
+	
 }
