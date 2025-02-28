ff8f8d9fa59f156dad63ae4721ed274c2e615191
Александр Смирнов <smirnov.ai@mmtr.ru>
2025-02-28 12:43:40 +0300

    Execution file changes after added com.wordle directiory

--- Execute.bat
+++ Execute.bat
@@ -1,5 +1,8 @@
 del out\* /s /q
+del sources.txt
 
-cmd /c javac -sourcepath .;./src/ -cp ./out/;./lib/gson-2.12.1.jar -d ./out/ Main.java && java -cp ./out/;./lib/gson-2.12.1.jar Main
+cmd /c dir /b /s *.java > sources.txt
+
+cmd /c javac -d ./out/ -cp .;./lib/gson-2.12.1.jar @sources.txt && java -cp ./out/;./lib/gson-2.12.1.jar Main
 
 timeout /T -1
